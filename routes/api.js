2ea4e171ce9d95603aeb02aa1b9ca46abd16aa0f
realprinceviper <idrisadeniji01@gmail.com>
2019-12-09 16:13:17 -0500

    send test mail

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+let dotenv = require('dotenv');
+dotenv.config();
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -8,17 +13,30 @@ router.get('/', function(req, res, next) {
 router.get('/:action', (req, res, next) => {
     let action = req.params.action;
     if(action === 'send') {
-        res.json({
-            confirmation: 'Success',
-            action: action
-        })
+        
+        const msg = {
+        to: 'idrisadeniji01@gmail.com',
+        from: 'noreply@email.com',
+        subject: 'Hello world',
+        text: 'hi',
+        html: '<p>Hello HTML world!</p>',
+        };
 
-        return
-    } else {
-        res.json({
-            confirmation: 'failed',
-            action: action
-        })
+        sgMail.send(msg);
+        
+        if(res.statusCode === 200) {
+            return res.json({
+                Confirmation: 'Email successfully sent.'
+            })
+        } else {
+            return res.json({
+                failure: 'Unable to send mail'
+            });
+        }
+        
+
+        return next();
     }
 })
+
 module.exports = router;
